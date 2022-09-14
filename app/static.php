cc0d96d00b971adefd246462c511508ebd308331
ValerieLeonidova <47154479+ValerieLeonidova@users.noreply.github.com>
2022-09-14 12:13:12 +0300

    files

--- app/static.php
+++ app/static.php
@@ -36,6 +36,63 @@ include('header.php');
                         <h5>Title goes here lorem ipsum dolor</h5>
                         <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequatur eligendi est fuga fugit laborum, libero nam nesciunt perspiciatis qui quod velit vero voluptatibus. Eius exercitationem laboriosam maxime repellat tempora.</span><span>Culpa dolore est maiores minus modi necessitatibus numquam quos rerum suscipit veritatis. At consequatur corporis, cupiditate doloribus incidunt ipsum maiores, maxime perferendis, possimus quia quis sapiente tempore? Fugiat, voluptatem, voluptatibus?</span>
                         </p>
+                        <p><strong>Files:</strong></p>
+                        <ul class="files">
+                            <li class="files__item">
+                                <a href="" class="files__link">
+                                    <strong>
+                                        <i class="far fa-file-alt mr-2"></i>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    </strong>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </li>
+                            <li class="files__item">
+                                <a href="" class="files__link">
+                                    <strong>
+                                        <i class="far fa-file-alt mr-2"></i>
+                                        Aliquid amet assumenda commodi cupiditate, dolorum facere fugit
+                                    </strong>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </li>
+                            <li class="files__item">
+                                <a href="" class="files__link">
+                                    <strong>
+                                        <i class="far fa-file-alt mr-2"></i>
+                                        Blanditiis eveniet molestiae officiis rerum voluptatem?
+                                    </strong>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </li>
+                            <li class="files__item">
+                                <a href="" class="files__link">
+                                    <strong>
+                                        <i class="far fa-file-alt mr-2"></i>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    </strong>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </li>
+                            <li class="files__item">
+                                <a href="" class="files__link">
+                                    <strong>
+                                        <i class="far fa-file-alt mr-2"></i>
+                                        Aliquid amet assumenda commodi cupiditate, dolorum facere fugit
+                                    </strong>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </li>
+                            <li class="files__item">
+                                <a href="" class="files__link">
+                                    <strong>
+                                        <i class="far fa-file-alt mr-2"></i>
+                                        Blanditiis eveniet molestiae officiis rerum voluptatem?
+                                    </strong>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </li>
+                        </ul>
                     </article>
                 </div>
             </div>
