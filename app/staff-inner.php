016c05dae15036be0266e7ce85b0f086c0a819b0
ValerieLeonidova <47154479+ValerieLeonidova@users.noreply.github.com>
2022-09-28 16:15:49 +0300

    Staff

---
+++ app/staff-inner.php
@@ -0,0 +1,48 @@
+
+<?php include 'head.php'?>
+
+<?php
+$page = 'home';
+include('header.php');
+?>
+
+<article class="banner banner--page banner--bottom" style="background-image: url(./img/demo/bottom-image.jpg)">
+    <div class="banner-text wow zoomIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+        <div class="container">
+            <h1 class="ttl">Staff</h1>
+        </div>
+    </div>
+</article>
+
+<main class="page">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10">
+                <div class="page-box">
+                    <article class="page-static">
+                        <div class="text-center">
+                            <div class="staff__photo">
+                                <img src="./img/no-image.png" alt="">
+                            </div>
+                            <h2 class="staff__name">Robert Blanch </a></h2>
+                            <p class="staff__position">Founding Principal</p>
+                            <p class="staff__email"><a href="mailto:">RBlanch@materacademy.com</a></p>
+                        </div>
+                        <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis dicta illo ipsum nisi repudiandae, voluptatum? Autem culpa, deleniti ducimus excepturi illo ipsa magnam maxime minima nam nihil quidem suscipit!</span><span>Ad consectetur id iusto maxime mollitia pariatur possimus soluta sunt ullam? Amet autem consectetur consequuntur corporis debitis dolor eius fugiat hic ipsam magni, ratione reiciendis sapiente sint soluta tempore unde.</span>
+                        </p>
+                        <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet at et expedita, id molestias numquam officia pariatur perspiciatis quis reiciendis rem reprehenderit, saepe similique tempore. Doloremque dolores id voluptatum!</span><span>Amet deserunt laboriosam nemo vero voluptatum? Quas, tempore tenetur? Adipisci aliquid aperiam earum, enim facilis fugit incidunt iure labore nemo nesciunt non officia similique unde voluptas, voluptate! Nihil numquam, obcaecati?</span><span>Corporis distinctio, dolorem eos et hic numquam quia tempora ullam? Aut autem eos esse est iusto modi, praesentium. Dolore doloremque dolorum ducimus eum eveniet illum laborum libero quam quisquam voluptatibus.</span>
+                        </p>
+                        <div class="d-flex justify-content-end border-top pt-3">
+                            <a href="staff.php" class="btn btn--primary-outline"><i class="fas fa-angle-left"></i> Back</a>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img src="./img/svg/Mater_Columbus_Ms_icon2.svg" alt="" class="page-icon">
+</main>
+
+<?php include 'footer.php' ?>
+
+<?php include 'foot.php' ?>
---
+++ app/staff.php
@@ -0,0 +1,140 @@
+
+<?php include 'head.php'?>
+
+<?php
+$page = 'home';
+include('header.php');
+?>
+
+<article class="banner banner--page banner--bottom" style="background-image: url(./img/demo/bottom-image.jpg)">
+    <div class="banner-text wow zoomIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+        <div class="container">
+            <h1 class="ttl">Staff</h1>
+        </div>
+    </div>
+</article>
+
+<main class="page">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10">
+                <div class="page-box">
+                    <article class="page-static">
+                        <div class="d-flex justify-content-end mb-3">
+                            <div class="search search--title">
+                                <input type="text" class="search__text" placeholder="Search">
+                                <button class="search__button"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                        <div class="staff__title wow fadeInUp" data-wow-duration="1.5s">
+                            <h2>ADMINISTRATION </h2>
+                        </div>
+                        <div class="staff__list">
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Robert Blanch </a></h3>
+                                <p class="staff__position">Founding Principal</p>
+                                <p class="staff__email"><a href="mailto:">RBlanch@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Cesar Christian Crousillat</a></h3>
+                                <p class="staff__position">Assistant Principal</p>
+                                <p class="staff__email"><a href="mailto:">Cesar@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Kim Guilarte</a></h3>
+                                <p class="staff__position">Instructional Coach</p>
+                                <p class="staff__email"><a href="mailto:">Guilarte@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Maria Beatriz Nunez</a></h3>
+                                <p class="staff__position">Dean of Students</p>
+                                <p class="staff__email"><a href="mailto:">Maria-Beatriz@materacademy.com</a></p>
+                            </div>
+                        </div>
+                        <div class="staff__title wow fadeInUp" data-wow-duration="1.5s">
+                            <h2>KINDERGARTEN TEACHERS </h2>
+                        </div>
+                        <div class="staff__list">
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Robert Blanch </a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">RobertBlanch@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Cesar Christian Crousillat</a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">Cesar.Christian@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Kim Guilarte</a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">Kim_Guilarte@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Maria Beatriz Nunez</a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">Maria-Beatriz@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Kim Guilarte</a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">Kim_Guilarte@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Maria Beatriz </a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">Maria-Beatriz@materacademy.com</a></p>
+                            </div>
+                            <div class="staff__item wow fadeInUp" data-wow-duration="1.5s">
+                                <a href="staff-inner.php" class="staff__photo">
+                                    <img src="./img/no-image.png" alt="">
+                                </a>
+                                <h3 class="staff__name"><a href="">Kim Guilarte</a></h3>
+                                <p class="staff__position">Teacher</p>
+                                <p class="staff__email"><a href="mailto:">Kim_Guilarte@materacademy.com</a></p>
+                            </div>
+                        </div>
+                        <div class="text-center mt-5 wow fadeInUp" data-wow-duration="1.5s">
+                            <a href="#" class="btn btn--primary-outline">Show More</a>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img src="./img/svg/Mater_Columbus_Ms_icon2.svg" alt="" class="page-icon">
+</main>
+
+<?php include 'footer.php' ?>
+
+<?php include 'foot.php' ?>
